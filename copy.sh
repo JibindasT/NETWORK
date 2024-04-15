16b03476033d8c2c9945554883c172261bed9c85
JIBIN <121844563+JibindasT@users.noreply.github.com>
2024-04-15 16:00:28 +0530

    Completed

---
+++ copy.sh
@@ -0,0 +1,27 @@
+
+#COPY SET OF FILES AS PAIRS
+
+
+if [ $# -ne 2 ]
+then
+echo " syntax is  <$0> <filename>"
+exit 1
+fi
+
+if [ -f $1 ]
+then
+echo " $1 is not existing or not an ordinary file "
+exit 2
+fi
+
+if [ -f $2 ]
+then
+echo " target file exists,overwrite it (y/n)"
+read ans
+if [ $ans = "n" ]
+then exit 3
+fi
+fi
+cp $1 $2
+echo "file copied"
+
