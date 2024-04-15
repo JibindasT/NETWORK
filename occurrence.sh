d12f93de47543f69994a01fe6d8606cb9c417a41
JIBIN <121844563+JibindasT@users.noreply.github.com>
2024-04-15 16:01:28 +0530

    Completed

---
+++ occurrence.sh
@@ -0,0 +1,29 @@
+
+#OCCURANCE OF WORDS
+
+
+if [ $# -eq 0 ]
+then
+echo " syntax is <$0> <word> <filename> "
+exit 1
+fi
+
+word=$1
+shift
+count=0
+
+while [ $# -ne 0 ]
+do
+for wd in `cat $1`
+do
+
+if [ $wd = $word ]
+then
+count=`expr $count + 1`
+fi
+done
+shift
+done
+echo " no of occurrence of words=$count"
+
+
